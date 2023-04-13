6c76b038af5ae23c9c69b67607c6bc980f640140
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-13 12:52:29 -0400

    got timer to work

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,31 @@
 // var answerChoices = document.querySelector("#answerChoices");
 // var startButton = document.querySelector("#startButton");
 // var timer = document.querySelector("#timer");
-var timeEl = document.querySelector('.time');
-var mainEl = document.getElementById('main');
-var secondsLeft = 90;
+// var time_limit = 90;
 
+
+
+//Countdown timer 
+const timeH = document.querySelector('h1');
+let timeSecond = 90;
+
+displayTime(timeSecond)
+
+const countDown = setInterval (()=> {
+    timeSecond--;
+    displayTime(timeSecond);
+    if(timeSecond <= 0 || timeSecond < 1) {
+        endTime();
+        clearInterval(countDown);
+    }
+},1000)
+
+function displayTime(second){
+    const min = Math.floor(second / 60);
+    const sec = Math.floor(second % 60);
+    timeH.innerHTML= `${min<10 ? '0': ''}${min}:${sec<10?'0':''}${sec}`
+}
+
+function endTime() {
+    timeH.innerHTML = 'time out'
+}
