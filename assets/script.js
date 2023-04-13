ab6133b396a6d995d3a85e5954c8ba9773dca6ef
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-04-13 10:19:26 -0400

    Cleared CSS and focusing on timer

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
-var answerChoices = document.querySelector("#answerChoices");
-var startButton = document.querySelector("#startButton");
-var timer = document.querySelector("#timer");
+// var answerChoices = document.querySelector("#answerChoices");
+// var startButton = document.querySelector("#startButton");
+// var timer = document.querySelector("#timer");
+var timeEl = document.querySelector('.time');
+var mainEl = document.getElementById('main');
+var secondsLeft = 90;
+
